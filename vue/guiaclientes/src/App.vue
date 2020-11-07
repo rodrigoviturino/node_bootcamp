9cd0bbd09451bef2ee275873d97ebdedafc15b3c
Rodrigo Viturino <Viturino_souza@outlook.com>
2020-11-07 14:39:39 -0300

    Init Commit

---
+++ vue/guiaclientes/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <cliente></cliente>
+    <cliente></cliente>
+    <cliente></cliente>
+    <cliente></cliente>
+  </div>
+</template>
+
+<script>
+import Cliente from './components/Cliente.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Cliente
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/guiaclientes/src/components/Cliente.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <h2>Seja Bem-Vindo</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cliente"
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 30px;
+  }
+</style>
