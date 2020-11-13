1872d5193aa078aa8db8677bbeeb33085db9e0af
Rodrigo Viturino <Viturino_souza@outlook.com>
2020-11-13 01:28:26 -0300

    Vue Projeto Pokedex - listando dados via api

--- vue/podekex/src/App.vue
+++ vue/podekex/src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <div id="app">
+    <div v-for="(poke,index) in pokemons" :key="index">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"></Pokemon>
+    </div>
   </div>
 </template>
 
 <script>
-//import axios from "axios";
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
 
 export default {
   name: 'App',
+  data(){
+    return {
+      pokemons: []
+    }
+  },
+  methods: {
+    apiPokemon(){
+      axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151&offset=0`)
+      .then((res) => {
+        this.pokemons = res.data.results;
+        console.log(this.pokemons);
+      })
+    }
+  },
+  components: {
+    Pokemon
+  },
+  created(){
+    this.apiPokemon()
+  }
 }
 </script>
 
---
+++ vue/podekex/src/components/Pokemon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h1>{{num}} {{name | upper}}</h1>
+      <small>{{url}}</small>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Pokemon",
+    props: {
+        num: Number,
+        name: String,
+        url: String,
+    },
+    filters: {
+        upper(value){
+            return value.charAt(0).toUpperCase() + value.slice(1)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
